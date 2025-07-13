00205e30d0a994e0c28db1c3597d33851f785cbe
Vu Nguyen Hoang <contact.vunguyenhoang@gmail.com>
2025-07-13 10:47:53 +0200

    Update Init_Database.sql

--- Scripts/Init_Database.sql
+++ Scripts/Init_Database.sql
@@ -1,3 +1,22 @@
+/*
+=========================================
+Create Data Base and Schemas
+=========================================
+Script Purpose:
+
+  This script creates a new database named "DataWarehouse" after checking if it already exists.
+  If the database exists, it's dropped and recreated. Additionally, the script sets up three
+  schemas within the database: "bronze", "silver" & "gold".
+
+WARNING:
+
+  Running this script will drop the entire "DataWarehouse" database if it exists.
+  All data in the database will be permanently deleted. Proceed with caution and ensure proper
+  backups before running the script.
+*/
+
+
+
 -- Drop database if it exists
 DROP DATABASE IF EXISTS datawarehouse;
 
